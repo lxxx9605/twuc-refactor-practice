db338e8eb35a841966c2b54abd49b2b4735fcd13
xiao <535408893@qq.com>
2021-08-11 22:56:39 +0800

    calculatetaxi charges in Taxi class

--- 5/src/main/java/com/twu/refactoring/Taxi.java
+++ 5/src/main/java/com/twu/refactoring/Taxi.java
@@ -1,14 +1,34 @@
 package com.twu.refactoring;
 
 public class Taxi {
-    private boolean airConditioned;
+    private final boolean airConditioned;
     private final int totalKms;
     private final boolean peakTime;
+    private static final double PEAK_TIME_MULTIPLIER = 1.2;
+    private static final double OFF_PEAK_MULTIPLIER = 1.0;
+    private static final int RATE_CHANGE_DISTANCE = 10;
+    private static final int PRE_RATE_CHANGE_NON_AC_RATE = 15;
+    private static final int POST_RATE_CHANGE_NON_AC_RATE = 12;
+    private static final int PRE_RATE_CHANGE_AC_RATE = 20;
+    private static final int POST_RATE_CHANGE_AC_RATE = 17;
 
     public Taxi(boolean airConditioned, int totalKms, boolean peakTime) {
         this.airConditioned = airConditioned;
         this.totalKms = totalKms;
         this.peakTime = peakTime;
+    }
+
+    public double calculateTaxiCharges (double totalCost) {
+        int totalKms = this.getTotalKms();
+        double peakTimeMultiple = this.isPeakTime() ? PEAK_TIME_MULTIPLIER : OFF_PEAK_MULTIPLIER;
+        if(this.isAirConditioned()) {
+            totalCost += Math.min(RATE_CHANGE_DISTANCE, totalKms) * PRE_RATE_CHANGE_AC_RATE * peakTimeMultiple;
+            totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * POST_RATE_CHANGE_AC_RATE * peakTimeMultiple;
+        } else {
+            totalCost += Math.min(RATE_CHANGE_DISTANCE, totalKms) * PRE_RATE_CHANGE_NON_AC_RATE * peakTimeMultiple;
+            totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * POST_RATE_CHANGE_NON_AC_RATE * peakTimeMultiple;
+        }
+        return totalCost;
     }
 
     public boolean isAirConditioned() {
